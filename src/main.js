053a3c9a1b53a5ac1c3560d17511f878661a1e99
morshed <maraju88@yahoo.com>
2020-07-22 11:07:38 +0600

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.config.devtools = true;
+// After you create app
+//window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = app.constructor;
 
 new Vue({
   render: h => h(App),
